88e891ee2bfce13aacc6cc14a0037110569d0db6
sangjunni <yoonsj0322@gmail.com>
2024-02-20 17:52:25 +0900

    [Refactor] 주석 변경

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     Crawler = ArticleCrawler()
     Crawler.set_category("IT과학")
     date = datetime(2024,1,1)
-    # 병렬 처리를 진행할 경우 금방 limit에 도달 -> 1일 단위는 어느 정도 잘 수집됨
+    
     for i in range(50):
         d2s = date.strftime("%Y") +'-' + date.strftime("%m") +'-' +date.strftime("%d")
         Crawler.set_date_range(d2s, d2s)
--- news_crawler.py
+++ news_crawler.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     Crawler = ArticleCrawler()
     Crawler.set_category("IT과학")
     date = datetime(2024,1,1)
-    # 병렬 처리를 진행할 경우 금방 limit에 도달 -> 1일 단위는 어느 정도 잘 수집됨
+    # 병렬 처리를 진행할 경우 금방 limit에 도달 -> 1일 단위는 잘 수집됨
+    # 만약 원격 호스트 연결와의 연결이 끊기는 현상 발생시 articlecrawler.py 167line의 sleep 값 늘리기
     for i in range(50):
         d2s = date.strftime("%Y") +'-' + date.strftime("%m") +'-' +date.strftime("%d")
         Crawler.set_date_range(d2s, d2s)
